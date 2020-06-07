35b0d7844ee3317c38b284f562815d6aa271cf2c
Marek <marek5886@hotmail.com>
2020-06-07 23:41:36 +0200

    added very basic text fitting algorithm to the text component

--- sandbox/srcs/main.cpp
+++ sandbox/srcs/main.cpp
@@ -1,31 +1,33 @@
 #include "../../includes/mfGUI.hpp"
 
-void CreateMainMenu(bool *isOpen, bool *isRecreate)
+void CreateMainMenu(bool *isOpen)
 {
 	mf::GUI::ClearWidgets();
 
 	mf::Button	*btn = mf::Button::Create(sf::Color::Blue, sf::Color::Cyan);
-	btn->SetPosition(sf::Vector2f(1480, 20))
+	btn->SetPosition(1300, 50)
+	->SetSize(250, 50)
 	->SetTextFont("assets/Bangers-Regular.ttf")
-	->SetText("Quit!")
-	->SetClickEvent([isOpen, isRecreate] {
-		*isRecreate = true;
-		std::cout << "I was reset!" << std::endl;
+	->SetTextColor(sf::Color::Black)
+	->SetText("This is a test to check the line cutting feature!\n I think it works fine, let's test this shit with a slightly longer text shall we?!")
+	->SetClickEvent([isOpen] {
+		*isOpen = false;
 	});
 	mf::GUI::AddWidget(btn);
+	mf::GUI::AddWidget(mf::Image::Create("assets/photo-1542044896530-05d85be9b11a.jpeg")
+	->SetSize(700.f, 500.f)
+	->SetPosition(100.f, 50.f)
+	->SetOutlineColor(sf::Color::Black)
+	->SetOutlineThickness(3.f));
 }
-
 
 int main()
 {
 	sf::RenderWindow	window(sf::VideoMode(1600, 900), "Sandbox", sf::Style::Default);
 	mf::GUI::Init(&window);
 	bool	isOpen = true;
-	bool	isRecreate = false;
 
-	CreateMainMenu(&isOpen, &isRecreate);
-
-	
+	CreateMainMenu(&isOpen);
 
 	int fps = 0;
 	sf::Clock fpsClock;
@@ -43,11 +45,6 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				isOpen = false;
-			if (isRecreate)
-			{
-				CreateMainMenu(&isOpen, &isRecreate);
-				isRecreate = false;
-			}
 			mf::GUI::HandleEvent(event);
 		}
 		window.clear(sf::Color::White);
